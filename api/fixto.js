fafb9584a8a59d817c9f66fc462dcebd10c7250f
RizMax001 <awsstok@gmail.com>
2025-10-20 17:51:42 +0700

    Update fixto.js

--- api/fixto.js
+++ api/fixto.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const nodemailer = require('nodemailer')
 
 const CONFIG = {
-  API_KEY: 'ror',
+  API_KEY: 'kontol',
   SMTP_HOST: 'smtp.gmail.com',
   SMTP_PORT: 465,
   SMTP_SECURE: true,
